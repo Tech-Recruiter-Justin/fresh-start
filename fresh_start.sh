2acdfb2d2b57a9849d2ed12bd5e3c0efddbad2e3
Justin Choi <justinchoi@Justins-MacBook-Pro.local>
2022-10-15 20:14:38 +0800

    initial commit

---
+++ fresh_start.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+dev_path="${HOME}/Dev"
+
+casks=("google-chrome" "clipy" "spectacle" "iterm2" "visual-studio-code" "notion" "sublime-text" "jetbrains-toolbox" "postman" "alfred")
+
+packages=("git" "node" "python" "openjdk@17" "dockutil" "neovim")
+
+installed=$(brew list --versions)
+
+# Create Dev directory
+if [ -d ${dev_path} ] 
+then
+    echo "Directory ${dev_path} exists." 
+else
+    mkdir ${dev_path}
+    echo "Directory ${dev_path} does not exists. Creation completed."
+fi
+
+# Install Brew
+which -s brew
+if [ $? != 0 ]
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "Installed Brew"
+else
+    brew update
+    echo "Brew exists. Updated Brew."
+fi
+
+# Install Brew casks
+for cask in "${casks[@]}"
+do
+    $(echo ${installed} | grep ${cask} &>/dev/null)
+    if [ $? != 0 ]
+    then
+        brew install --cask ${cask} 
+    else
+        echo "${cask} exists."
+    fi
+done
+
+# Install Brew packages
+for package in "${packages[@]}"
+do
+    $(echo ${installed} | grep ${package} &>/dev/null)
+    if [ $? != 0 ]
+    then
+        brew install ${package} 
+    else
+        echo "${package} exists."
+    fi
+done
+
+# Install Oh My Zsh
+if [ -d ${HOME}/.oh-my-zsh ]
+then
+    echo "Oh My Zsh exists."
+else
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "Installed Oh My Zsh"
+fi
+
+# Clean up dock
+dockutil --remove all
+dockutil --add $HOME/Applications/JetBrains\ Toolbox/IntelliJ\ IDEA\ Ultimate.app
+dockutil --add $HOME/Applications/JetBrains\ Toolbox/PyCharm\ Professional.app
+dockutil --add $HOME/Applications/JetBrains\ Toolbox/DataGrip.app
+dockutil --add /Applications/Postman.app
+dockutil --add /Applications/Sublime\ Text.app
+dockutil --add /Applications/Visual\ Studio\ Code.app
+dockutil --add /Applications/Google\ Chrome.app
+dockutil --add /Applications/Notion.app
+dockutil --add /Applications/iTerm.app
+
+echo "++++ Installation completed ++++"
+
